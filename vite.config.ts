71e25ee898a810b17c8644f47b9ed39ccd9d117d
sugasaki <sugasaki@gmail.com>
2025-04-28 00:49:55 +0900

    Configure relative path and add Vite logo to public directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: './', // 相対パスを使う
   plugins: [react()],
 });
